4d6637698e3ee4573b20a1559124a27e52710b2e
Osmanys Fuentes L <ossmanys@gmail.com>
2024-11-25 17:04:41 +0400

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+
+	const { children } = $props();
+</script>
+
+<svelte:head>
+	<title>GoodValue</title>
+	<meta name="description" content="GoodValue online store" />
+</svelte:head>
+
+<div>
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li>
+				<a href="/products">Products</a>
+			</li>
+			<li>
+				<a href="/categories">Categories</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+<section>
+
+</section>
